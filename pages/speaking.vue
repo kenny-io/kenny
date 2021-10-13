a7bef08782975879a6ebee29f7ef028f70c82ae1
kenny-io <ekeneeze3@gmail.com>
2021-10-14 01:06:07 +0400

    added talks content and search

--- pages/speaking.vue
+++ pages/speaking.vue
@@ -30,18 +30,31 @@
         </div>
       </div>
     </div>
+    <form class="flex justify-center mt-10">
+      <div class="form-control w-90">
+        <div class="relative">
+          <input
+            type="search"
+            @input="$fetch"
+            v-model="query"
+            placeholder="Search Talks"
+            class=" pr-16 input input-bordered border-2 border-gray-300 text-gray-300"
+          />
+        </div>
+      </div>
+    </form>
     <section class=" body-font">
       <div class="container px-5 py-12 mx-auto">
         <div class="flex flex-wrap -m-4">
           <div v-for="talk in talks" :key="talk.title" class="p-4 md:w-1/3">
             <div class="card shadow-xl overflow-hidden w-full max-w-lg ">
               <figure class=" px-10 pt-10">
-                <img class=" rounded-xl  " src="../static/kenny-3.jpg" />
+                <img class="rounded-xl" src="../static/kenny-3.jpg" />
               </figure>
               <div class="justify-end card-body text-primary-content">
-                <h2 class="card-title ">{{ talk.title }}</h2>
+                <h2 class="card-title ">{{ talk.conference }}</h2>
                 <p class="opacity-80">
-                  {{ talk.description }}
+                  {{ shortDescription(talk.description) }}
                 </p>
                 <div class="card-actions">
                   <app-button :title="`Watch Talk`" />
@@ -63,29 +76,25 @@
 </style>
 
 <script>
-import AppButton from "~/components/AppButton.vue";
 export default {
-  components: { AppButton },
   data() {
     return {
-      talks: [
-        {
-          title: "VueConf London 21",
-          description:
-            "Rerum reiciendis beatae tenetur excepturi aut pariatur est eos. Sit sit necessitatibus veritatis sed molestiae voluptates incidunt iure sapiente."
-        },
-        {
-          title: "Concatenate Conf 19",
-          description:
-            "Rerum reiciendis beatae tenetur excepturi aut pariatur est eos. Sit sit necessitatibus veritatis sed molestiae voluptates incidunt iure sapiente."
-        },
-        {
-          title: "InfoBit Shift 21",
-          description:
-            "Rerum reiciendis beatae tenetur excepturi aut pariatur est eos. Sit sit necessitatibus veritatis sed molestiae voluptates incidunt iure sapiente."
-        }
-      ]
+      talks: "",
+      query: ""
     };
+  },
+  async fetch() {
+    this.talks = await this.$content("talks")
+      .only(["title", "description", "conference"])
+      .sortBy("date")
+      .search(this.query)
+      .fetch();
+  },
+  methods: {
+    // truncate the description
+    shortDescription(description) {
+      return description.toString().substring(0, 100) + "...";
+    }
   }
 };
 </script>
