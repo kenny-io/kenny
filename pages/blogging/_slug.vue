f1c4b70b15899278c77a9b7f57202e49d174274c
kenny-io <ekeneeze3@gmail.com>
2021-10-15 16:11:06 +0400

    took a stab at making blog reading page responsive

--- pages/blogging/_slug.vue
+++ pages/blogging/_slug.vue
@@ -1,11 +1,11 @@
 <template>
-  <section data-theme="kennylight" class="">
+  <section data-theme="kennylight">
     <div class="container px-5 py-24 mx-auto">
       <div
-        class="lg:w-2/3 flex flex-col sm:flex-row sm:items-center items-start mx-auto"
+        class="lg:w-2/3 flex flex-col sm:flex-row sm:items-center items-start mx-auto overflow-auto"
       >
         <nuxt-content
-          class="prose prose-sm sm:prose lg:prose-lg xl:prose-2xl mx-auto text-primary-content"
+          class="prose prose-sm sm:prose lg:prose-lg xl:prose-2xl"
           :document="post"
         ></nuxt-content>
       </div>
@@ -17,19 +17,8 @@
 export default {
   async asyncData({ $content, params }) {
     const post = await $content("blog", params.slug).fetch();
-    console.log(params);
+
     return { post };
   }
 };
 </script>
-
-<style>
-/* .nuxt-content h2 {
-  font-weight: bold;
-  font-size: 20px;
-  margin: 15px 0 10px 0;
-}
-.nuxt-content p {
-  margin-top: 10px;
-} */
-</style>
